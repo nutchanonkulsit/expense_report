0efcd0276fe1949ce57f0ff93a7ddafc6112c65b
Nutchanon Kullasit <nutchanonkulsit@gmail.com>
2025-08-13 16:39:32 +0700

    create frontend and backend

---
+++ backend/src/main/java/com/spring/expense_report/ExpenseReportApplication.java
@@ -0,0 +1,13 @@
+package com.spring.expense_report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseReportApplication.class, args);
+	}
+
+}
