d02ff7bc885f8b9726006b8171f6b0de3126c083
Nutchanon Kullasit <nutchanonkulsit@gmail.com>
2025-09-05 23:29:52 +0700

    create home page

---
+++ frontend/src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: false,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {
+  constructor() {}
+
+  ngOnInit(): void {
+    // Initialization logic here
+  }
+
+
+
+}
