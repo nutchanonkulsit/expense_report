fc9e50ae696523516f336578e721de24804d8b03
Nutchanon Kullasit <nutchanonkulsit@gmail.com>
2025-09-16 15:00:15 +0700

    layout navbar

--- frontend/src/app/layouts/nav-bar/nav-bar.component.ts
+++ frontend/src/app/layouts/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -10,6 +11,7 @@ export class NavBarComponent {
   @ViewChild('header') headerRef!: ElementRef;
   headerHeight: number = 0;
 
+  constructor(private router: Router) {}
   ngAfterViewInit(): void {
     setTimeout(() => {
       this.updateHeaderHeight();
@@ -23,7 +25,12 @@ export class NavBarComponent {
 
   updateHeaderHeight() {
     if (this.headerRef) {
-      this.headerHeight = this.headerRef.nativeElement.offsetHeight ;
+      this.headerHeight = this.headerRef.nativeElement.offsetHeight;
     }
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
   }
 }
